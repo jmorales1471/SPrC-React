0845c6a9d199742909b7b9b493c63d0b6ee26f95
John Morales <morales.201@osu.edu>
2018-08-25 19:01:28 -0400

    Set up testing

---
+++ tests/setup-spec.js
@@ -0,0 +1,7 @@
+import {expect} from 'chai';
+
+describe("Set Up Mocha and Chai", () => {
+    it("should pass test if mocha is set up correctly", () => {
+        expect(true).to.be.true;
+    });
+});
